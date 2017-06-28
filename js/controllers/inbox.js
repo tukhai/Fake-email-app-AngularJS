58b6ca3e86489a1273571abae391849a893832ea
NguyenTuKhai <ntukhai@gmail.com>
2017-06-28 19:08:01 +0800

    Using factories for inbox to make GET request to emails.jon file

--- js/controllers/inbox.js
+++ js/controllers/inbox.js
@@ -2,8 +2,25 @@
  * Controller: InboxCtrl
  */
 angular.module('EmailApp')
-    .controller('InboxCtrl',
+    /*.controller('InboxCtrl',
 		function InboxCtrl ( ) {
 			'use strict';
 			console.log('Inbox Loaded!')
+		});*/
+		
+	.controller('InboxCtrl',
+		function InboxCtrl ( $scope, InboxFactory ) {
+			'use strict';
+			$scope.meta = {
+				title: "My Inbox"
+			};
+			InboxFactory.getMessages()
+				.success(function(jsonData, statusCode){
+					console.log('The request was successful!', statusCode);
+					console.dir(jsonData);
+					// Now add the Email messages to the controller's scope
+					$scope.data = {
+					    emails: jsonData
+					};
+				});
 		});
